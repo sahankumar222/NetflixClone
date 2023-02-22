310aca0ee87210cce3cb93c3fbc60fa1beb94e42
sahankumar222 <sahankumar222@gmail.com>
2023-02-23 01:37:42 +0530

    step-3

--- src/App.js
+++ src/App.js
@@ -1,24 +1,15 @@
 import "./App.css";
 import Navbar from "./components/Navbar/Navbar";
+import Banner from './components/Banner/Banner';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
     
-        {/* <p>
-          Netflix Clone
-        </p> */}
-        {/* <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          i
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a> */}
+    
         <Navbar/>
+        <Banner/>
       </header>
     </div>
   );
--- src/components/Banner/Banner.js
+++ src/components/Banner/Banner.js
@@ -6,10 +6,42 @@ import requests from "../../request";
 const Banner = () => {
   const [movie, setMovie] = useState([]);
   useEffect(() => {
+    async function fetchData() {
+      const response = await axios.get(requests.fetchTrending);
+      console.log(response.data);
+      setMovie(
+        response.data.results[
+          Math.floor(Math.random() * response.data.results.length - 1)
+        ]
+      );
+    }
+    fetchData();
 
   }, []);
 
   return (
+     <div>
+      <header
+        className="banner"
+        style={{
+          backgroundSize: 'cover',
+          backgroundPosition: 'center center',
+          backgroundImage: `url(https://image.tmdb.org/t/p/original/${movie?.backdrop_path})`
+        }}
+      >
+        <div className="banner__contents">
+          <h1 className="banner__title">
+            {movie?.title || movie?.name || movie?.original_name}
+          </h1>
+          <p className="banner__description">
+            {movie?.overview}
+          </p>
+          <button className="banner__button">Play</button>
+          <button className="banner__button2">My Lists</button>
+        </div>
+      </header>
+    </div>
+    
 
   );
 };
